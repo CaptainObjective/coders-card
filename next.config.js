2ea8cbeb023b98e57844a76cc33c1ff90b68c1c7
KonradSzwarc <konrad.szwarc.dev@gmail.com>
2021-04-10 21:20:31 +0200

    Add Next.js Netlify plugin

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ module.exports = {
   future: {
     webpack5: true,
   },
-  images: {
-    imageSizes: [16, 20, 24, 32, 40, 48, 64, 80, 96, 128, 160, 200, 256, 280, 320, 360, 400, 600],
-  },
+  target: 'serverless',
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,
