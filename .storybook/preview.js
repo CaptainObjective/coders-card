c78063f3a8586bc0d44d78327bb8f7cfc1557ee2
KonradSzwarc <konrad.szwarc.dev@gmail.com>
2021-04-10 21:14:56 +0200

    Customize Storybook to handle next/image

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,17 @@
+import 'storybook-formik/register';
 import React from 'react';
 import { Box } from '@material-ui/core';
-import 'storybook-formik/register';
+import * as NextImage from 'next/image';
 import { addDecorator } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
 import { withNextRouter } from 'storybook-addon-next-router';
 import { ThemeProvider } from '../src/utils/theme';
+
+const OriginalNextImage = NextImage.default;
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
